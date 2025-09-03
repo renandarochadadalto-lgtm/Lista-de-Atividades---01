d5d60d4c6e7d16d807a9d2b36a9183a579390f3f
Renanda Rocha Dadalto <renandarochadadalto@gmail.com>
2025-09-03 11:19:29 -0300

    Feat: Atividade 06

---
+++ Atividade 06/Atividade 06/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Digite o salário mensal atual: R$ ");
+double salarioAtual = double.Parse(Console.ReadLine());
+Console.Write("Digite o percentual de reajuste (%): ");
+double percentualReajuste = double.Parse(Console.ReadLine());
+double novoSalario = salarioAtual * (1 + percentualReajuste / 100);
+Console.WriteLine($"\nO novo salário é: R$ {novoSalario:F2}");
