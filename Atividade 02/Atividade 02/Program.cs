a9db6dca8fcfe692656631d5530bd78c6e8b1cf3
Renanda Rocha Dadalto <renandarochadadalto@gmail.com>
2025-09-03 10:40:20 -0300

    Feat: Atividade 02

---
+++ Atividade 02/Atividade 02/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+int valor;
+Console.WriteLine("Digite um valor: ");
+valor = int.Parse(Console.ReadLine());
+Console.WriteLine($"o número antecessor é: {valor - 1}");
