f4e4d959df8ecc133ab77c39265c9f5e2c9b0991
Renanda Rocha Dadalto <renandarochadadalto@gmail.com>
2025-09-03 10:36:10 -0300

    Feat: Atividade 1

---
+++ Atividade 01/Atividade 01/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+int A = 10;
+int B = 20;
+int aux;
+Console.WriteLine($"O valor de A = {A} e o de B = {B}");
+aux = A;
+A = B;
+B = aux;
+Console.WriteLine($"O valor de A = {A} e o de B = {B}");
+Console.ReadLine();
