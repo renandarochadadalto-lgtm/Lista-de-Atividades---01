e36e215509a6098f74a02f4dd0d46c9d193f33a2
Renanda Rocha Dadalto <renandarochadadalto@gmail.com>
2025-09-03 10:43:16 -0300

    Feat: Atividade 03

---
+++ Atividade 03/Atividade 03/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+int B;
+int A;
+Console.WriteLine("Escreva as dimensões do retângulo: ");
+B = int.Parse(Console.ReadLine());
+A = int.Parse(Console.ReadLine());
+Console.WriteLine($"A área do retângulo é: {A * B}");
