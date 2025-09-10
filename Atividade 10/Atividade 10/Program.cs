7013f607b6a66b68f82e62c6c20b90f8391b9461
Renanda Rocha Dadalto <Renandarochadadalto@gmail.com>
2025-09-10 08:49:28 -0300

    Feat: Atividade 10

---
+++ Atividade 10/Atividade 10/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Digite a nota da 1ª avaliação: ");
+double nota1 = double.Parse(Console.ReadLine());
+
+Console.Write("Digite a nota da 2ª avaliação: ");
+double nota2 = double.Parse(Console.ReadLine());
+
+double media = (nota1 + nota2) / 2;
+
+Console.WriteLine($"\nA média do aluno é: {media:F2}");
+
+if (media >= 6.0)
+{
+	Console.WriteLine("O aluno foi APROVADO!");
+}
+else
+{
+	Console.WriteLine("O aluno NÃO FOI APROVADO.");
+}
