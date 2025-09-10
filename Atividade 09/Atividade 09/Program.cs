c1026658cf57b9a4d72f55cae76b854060638888
Renanda Rocha Dadalto <Renandarochadadalto@gmail.com>
2025-09-10 08:38:51 -0300

    Feat: Atividade 09

---
+++ Atividade 09/Atividade 09/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Digite o número de maçãs compradas: ");
+int numMacas = int.Parse(Console.ReadLine());
+
+double preco;
+
+if (numMacas < 12)
+{
+	preco = 1.30;
+}
+else
+{
+	preco = 1.00;
+}
+
+double custoTotal = numMacas * preco;
+
+Console.WriteLine($"O custo total da compra é: R${custoTotal:F2}");
