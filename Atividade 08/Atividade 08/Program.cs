31ab7c9a3a2877fe486a78810613ae2b67c05c23
Renanda Rocha Dadalto <Renandarochadadalto@gmail.com>
2025-09-10 08:08:14 -0300

    Feat: Atividade 08

---
+++ Atividade 08/Atividade 08/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Digite um valor (não pode ser zero): ");
+int valor = int.Parse(Console.ReadLine());
+
+	if (valor == 0)
+	{
+		Console.WriteLine("Valor inválido! Zero não é permitido.");
+	}
+	else if (valor > 0)
+	{
+		Console.WriteLine("O valor é POSITIVO.");
+	}
+	else
+	{
+		Console.WriteLine("O valor é NEGATIVO.");
+	}
