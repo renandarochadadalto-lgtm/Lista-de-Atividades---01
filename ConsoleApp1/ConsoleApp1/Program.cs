82cff88cb79c07d1b67319cbb7e61e4f546e47aa
Renanda Rocha Dadalto <Renandarochadadalto@gmail.com>
2025-09-10 08:05:54 -0300

    feat: Atividade 07

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Digite um valor: ");
+int valor = int.Parse(Console.ReadLine());
+
+if (valor > 10)
+{
+	Console.WriteLine("É maior que 10");
+}
+else
+{
+	Console.WriteLine("Não é maior que 10 ");
+}
