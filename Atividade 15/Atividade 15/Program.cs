b5b5aaf8fd8dd8a54b44fb1b67534ad2aed92d6a
Renanda Rocha Dadalto <renandarochadadalto@gmail.com>
2025-09-16 08:27:34 -0300

    Feat: Atividade 15

---
+++ Atividade 15/Atividade 15/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Números entre 1000 e 2000 com resto 5 ao dividir por 11:");
+
+for (int i = 1000; i <= 2000; i++)
+{
+    if (i % 11 == 5)
+    {
+        Console.WriteLine(i);
+    }
+
+}
