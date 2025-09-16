0c6d1570669b276d67f2089be3b37093b80a621d
Renanda Rocha Dadalto <renandarochadadalto@gmail.com>
2025-09-16 08:10:43 -0300

    Feat: Atividade 14

---
+++ Atividade 14/Atividade 14/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Digite um número inteiro maior que zero: ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+    Console.WriteLine(i);
+}
+
