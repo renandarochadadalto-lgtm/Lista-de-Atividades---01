a6bfd79076bfde3f7f03be0c5b43f56adf2b2e7e
Renanda Rocha Dadalto <renandarochadadalto@gmail.com>
2025-09-16 08:03:40 -0300

    Feat: for while

---
+++ For- while/For- while/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num, resultado, valor;
+Console.WriteLine("Digite un número para a multiplicação: ");
+num = int.Parse(Console.ReadLine());
+Console.WriteLine("Até que valor irá a multiplicação? ");
+valor = int.Parse(Console.ReadLine());
+int contador = 1;
+while (contador <= valor)
+{
+    resultado = contador + num;
+    Console.WriteLine($"{num} x {contador} = {resultado}");
+    contador++;
+}
