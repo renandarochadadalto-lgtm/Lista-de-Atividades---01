35428adcd37a1a691808742694dd66918b5fb856
Renanda Rocha Dadalto <renandarochadadalto@gmail.com>
2025-09-16 08:51:41 -0300

    Feat: Atividade 16

---
+++ Atividade 16/Atividade 16/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Digite um número inteiro: ");
+int n = int.Parse(Console.ReadLine());
+
+if (n < 0)
+{
+    Console.WriteLine("Fatorial de número negativo não existe.");
+}
+else
+{
+    long fatorial = 1;
+
+    for (int i = 1; i <= n; i++)
+    {
+        fatorial *= i;
+    }
+
+    Console.WriteLine($"{n}! = {fatorial}");
+}
