f2e65dea075e626b6c0552652ba463c852655295
Renanda Rocha Dadalto <renandarochadadalto@gmail.com>
2025-09-30 07:57:21 -0300

    Feat: matriz e vetor

---
+++ Autoria/Autoria/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+{
+        public string Nome;
+public int Idade;
+    }
+
+    static void Main()
+{
+    List<Paciente> pacientes = new List<Paciente>();
+    string opcao = "";
+
+    while (opcao != "3")
+    {
+        Console.WriteLine("\n🏥 Cadastro Hospitalar");
+        Console.WriteLine("1) Cadastrar paciente\n2) Listar pacientes\n3) Sair");
+        Console.Write("Opção: ");
+        opcao = Console.ReadLine();
+
+        if (opcao == "1")
+        {
+            pacientes.Add(new Paciente
+            {
+                Nome = Console.ReadLineWithPrompt("Nome: "),
+                Idade = int.Parse(Console.ReadLineWithPrompt("Idade: "))
+            });
+            Console.WriteLine("✅ Paciente cadastrado!");
+        }
+        else if (opcao == "2")
+        {
+            foreach (var p in pacientes)
+                Console.WriteLine($"Nome: {p.Nome}, Idade: {p.Idade}");
+        }
+    }
+    Console.WriteLine("👋 Sistema encerrado.");
+}
+}
+
+static class Extensions
+{
+    public static string ReadLineWithPrompt(this Console c, string prompt)
+    {
+        Console.Write(prompt);
+        return Console.ReadLine();
+    }
---
+++ Matriz/Matriz/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] numeros = new int[5];
+
+for (int i = 0; i < 5; i++)
+{ 
+    Console.WriteLine($"Digite o {i+1}º numero:");
+        numeros[1]= int.Parse(Console.ReadLine());
+}
+for (int j = 0; j < 5; j++)
+{
+    Console.WriteLine($"o valor na posição [{j}] = {numeros[j]}");
+}
